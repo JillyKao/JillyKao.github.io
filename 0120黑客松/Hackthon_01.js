9287e420841532d97d5e6358975a802a36d7a26a
Jilly <snsd05300913@gmail.com>
2022-01-25 23:39:47 +0800

    Hackthon2022

--- 0120黑客松/Hackthon_01.js
+++ 0120黑客松/Hackthon_01.js
@@ -1,70 +1,61 @@
-var answer = [];
+var result = Math.floor(Math.random() * 100);
+        var small = 0;
+        var big = 100;;
         var times = 0;
-        var A = 0;
-        var B = 0;
-        var input = [];
 
-        var CreateNumber = document.getElementById("CreateNumber");
-        CreateNumber.onclick = function () {
-            answer = [];
-            while (answer.length < 4) {
-                var num = Math.floor(Math.random() * 9);
-                if (answer.indexOf(num) == -1) {
-                    console.log("before" + answer);
-                    answer += num;
-                    console.log(answer);
+        var Hint = document.getElementById("Hint");
+        var InputValue = document.getElementById("InputValue");
+        var GuessNumber = document.getElementById("GuessNumber");
+        var Back = document.getElementById("Back");
+
+
+
+
+
+
+        GuessNumber.onclick = function () {
+            var n = InputValue.value;
+            InputValue.value = "";
+
+            console.log("times =" + times);
+            if (times <= 5) {
+                if (result == n) {
+                    var TextCorrect = "你猜對了!!<br/>你總共猜了" + times + "次";
+                    Hint.innerHTML = TextCorrect;
+                }
+                else if (n > result) {
+                    times += 1;
+                    if (n < big) {
+                        big = n;
+                    }
+                    var TextSmaller = "再猜小一點,你總共猜了" + times + "次<br/>在" + small + "和" + big + "之間";
+                    Hint.innerHTML = TextSmaller;
+                }
+                else if (n < result) {
+                    if (n > small) {
+                        small = n;
+                    }
+                    times += 1;
+                    var TextBigger = "再猜大一點,你總共猜了" + times + "次<br/>在" + small + "和" + big + "之間";
+                    Hint.innerHTML = TextBigger;
                 }
             }
+            if (times >= 5) {
+                alert("你超過5次了 失敗!!");
+            }
         }
 
-
-        var WatchAnswer = document.getElementById("WatchAnswer");
-        WatchAnswer.onclick = function () {
-            alert(answer);
+        var Back = document.getElementById("Back");
+        Back.onclick = function () {
+            var stringmin = InputValue.value.slice(0, -1);
+            InputValue.value = stringmin;
         }
 
-        var Restart = document.getElementById("Restart");
-        Restart.onclick = function () {
-            GuessArea.innerHTML = "";
-        }
-
-
-
-
-        var GuessButton = document.getElementById("GuessButton");
-        GuessButton.onclick = function () {
-            var text = document.getElementById("inputText").value;
-            for (var i = 0; i <= 3; i++) {
-                input[i] = text[i];
+        var buttons = document.getElementsByTagName("button");
+        for (var i = 0; i <= buttons.length; i++) {
+            var button = buttons[i];
+            button.onclick = function () {
+                var btn = event.srcElement;
+                document.getElementById("InputValue").value += btn.innerText;
             }
-
-
-            for (var i = 0; i <= 3; i++) {
-                console.log(A + "A" + B + "B");
-                if (answer.indexOf(input[i]) != -1) {
-                    if (answer[i] == input[i]) {
-                        A = A + 1;
-                        console.log(A + "A" + B + "B");
-                    }
-                    else {
-                        B = B + 1;
-                        console.log(A + "A" + B + "B");
-                    }
-                }
-            }
-            var value = (A + "A" + B + "B");
-
-            var GuessArea = document.getElementById("GuessArea");
-
-            if (A == 4 || A == "4") {
-                var item = "<li class=\"list-group-item d-flex justify-content-between align-items-center listitembackcolor\"><span class=\"badge badge-success badge-pill pill-style\">" + A + "A" + B + "B" + "</span>" + text + "</li>";
-                alert("你猜對了!");
-            }
-            else {
-                var item = "<li class=\"list-group-item d-flex justify-content-between align-items-center listitembackcolor\"><span class=\"badge badge-secondary badge-pill pill-style errorcolor\">" + A + "A" + B + "B" + "</span>" + text + "</li>";
-            }
-            GuessArea.innerHTML += item;
-            A = 0;
-            B = 0;
-
         }
